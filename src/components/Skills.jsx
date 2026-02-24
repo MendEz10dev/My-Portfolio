bfb5384d24627e839130e0156822c8ad360a0701
aliant <aliant@estudiantes.uci.cu>
2026-02-24 11:08:29 -0500

    Arregle los hover de las skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,9 +6,7 @@ export function Skills ({icon, name, bgColor}) {
   return (
 
 
-
-    
-    <div className={`p-4 w-30 text-center transition-transform duration-100 hover:scale-105 hover:shadow-lg rounded-xl ${bgColor}`}>
+    <div className={`p-4 w-30 text-center rounded-xl transition-all duration-300 hover:scale-110 hover:shadow-[0_0_20px_rgba(255,165,0,0.6)] backdrop-blur-md ${bgColor}`}>
       <div className="flex flex-col items-center text-center">
           <i className="text-white text-2xl">{icon}</i>
         <h3 className="text-xl font-semibold text-gray-800 dark:text-white">{name}</h3>
